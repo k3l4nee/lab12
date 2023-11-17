3d56ae89c953e3ee0d3a0ed2a1afc998c7422005
Thy Tran <thyatran0113@gmail.com>
2023-11-17 11:09:23 -0800

    updated TreeNode class

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -2,9 +2,9 @@ package trees;
 
 import java.io.IOException;
 
-public class TreeException extends FamilyTree
+public class TreeException extends Throwable
 {
-	public TreeException(String s) throws IOException
+	public TreeException(String s)
 	{
 		super(s);
 	}
